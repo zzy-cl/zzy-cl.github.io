2187e280e128f6ba845606502b64925bab83c6c5
赵泽宇ZZY <1580574280@qq.com>
2023-04-27 23:39:05 +0800

    github action test

--- themes/butterfly/scripts/filters/post_lazyload.js
+++ themes/butterfly/scripts/filters/post_lazyload.js
@@ -8,9 +8,9 @@
 
 const urlFor = require('hexo-util').url_for.bind(hexo)
 
-function lazyload (htmlContent) {
+function lazyload(htmlContent) {
   const bg = hexo.theme.config.lazyload.placeholder ? urlFor(hexo.theme.config.lazyload.placeholder) : 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
-  return htmlContent.replace(/(<img.*? src=)/ig, `$1 "${bg}" data-lazy-src=`)
+  return htmlContent.replace(/(<img(?!.*?class[\t]*=[\t]*['"].*?no-lazy.*?['"]).*? src=)/ig, `$1 "${bg}" data-lazy-src=`)
 }
 
 hexo.extend.filter.register('after_render:html', function (data) {
